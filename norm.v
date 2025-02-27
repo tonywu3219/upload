b3b7bcc663af8a28170fdb300bb33c98a4d33a96
tonywu3219 <tonywu3219@gmail.com>
2025-02-27 00:58:40 -0800

    Update norm.v

--- norm.v
+++ norm.v
@@ -2,7 +2,6 @@ module norm (clk, in, out, div, wr, o_full, reset, o_ready);
 
 parameter bw = 4;
 parameter width = 1;
-parameter DIV_CYCLES = 3;  // Number of cycles for division operation
 
 input clk;
 input wr;
@@ -11,7 +10,7 @@ input reset;
 input [bw-1:0] in;
 output reg [2*bw-1:0] out;
 output o_full;
-output o_ready;
+output reg o_ready;
 
 wire [bw-1:0] fifo_out;
 wire empty;
@@ -19,18 +18,16 @@ wire full;
 wire [2*bw-1:0] div_out;
 reg [2*bw-1:0] sum_q;
 
-// Division state machine registers
-reg [1:0] div_state;
-reg [DIV_CYCLES-1:0] div_counter;
+// Pipeline registers for division
 reg div_in_progress;
-reg div_valid;
+reg [2:0] div_counter;  // Configurable delay counter
+reg [bw-1:0] fifo_data_reg;  // Register to hold FIFO output
 
-// Division result calculation
-assign div_out = {fifo_out, 8'b00000000} / sum_q;
+assign div_out = {fifo_data_reg, 8'b00000000} / sum_q;
 
 fifo_top #(.bw(bw), .width(width)) fifo_top_instance (
     .clk(clk),
-    .rd(div && !div_in_progress),  // Only read from FIFO when starting a new division
+    .rd(div),  // Original FIFO read control
     .wr(wr),
     .in(in),
     .out(fifo_out),
@@ -41,43 +38,35 @@ always @ (posedge clk) begin
     if (reset) begin
         sum_q <= 0;
         out <= 0;
-        div_state <= 0;
-        div_counter <= 0;
         div_in_progress <= 0;
-        div_valid <= 0;
+        div_counter <= 0;
+        o_ready <= 1;
+        fifo_data_reg <= 0;
     end
     else begin
         if (wr)
             sum_q <= sum_q + in;
-        
-        // Division state machine
-        case (div_state)
-            0: begin  // Idle state
-                if (div && !div_in_progress) begin
-                    div_in_progress <= 1;
-                    div_counter <= DIV_CYCLES-1;
-                    div_state <= 1;
-                    div_valid <= 0;
-                end
-            end
             
-            1: begin  // Division in progress
-                if (div_counter == 0) begin
-                    // Division complete
-                    out <= div_out;
-                    div_in_progress <= 0;
-                    div_valid <= 1;
-                    div_state <= 0;
-                end
-                else begin
-                    div_counter <= div_counter - 1;
-                end
+        // Division pipeline control
+        if (div && !div_in_progress) begin
+            // Capture FIFO output into register
+            fifo_data_reg <= fifo_out;
+            div_in_progress <= 1;
+            div_counter <= 3'd3;  // Set delay cycles (adjust as needed)
+            o_ready <= 0;
+        end
+        else if (div_in_progress) begin
+            if (div_counter == 0) begin
+                // Division complete after specified delay
+                out <= div_out;
+                div_in_progress <= 0;
+                o_ready <= 1;
             end
-        endcase
+            else begin
+                div_counter <= div_counter - 1;
+            end
+        end
     end
 end
-
-// Output ready signal
-assign o_ready = !div_in_progress;
 
 endmodule
